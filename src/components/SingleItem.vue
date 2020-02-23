84748b033d50fbc040eb28626ad8fa0c4e0fb27c
C4Jay <choolakejay@gmail.com>
2020-02-23 18:27:28 +0530

    single-item-request

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -2,27 +2,38 @@
 <v-container>
     <v-container v-if="img">
         <v-layout row>
-        <v-img :src="img"></v-img>
+        <v-img height="398" :src="img"></v-img>
         </v-layout>
-        <v-container v-if="img0">
-            <v-layout row>
+    </v-container>
+    
+    <v-container v-if="img0">
+        <v-layout row>
         <v-img :src="img0"></v-img>
-            </v-layout>
-        </v-container><!-- 
-        <v-img :src="imgs[2]"></v-img>
- -->
+        </v-layout>
+    </v-container>
+    
+    <v-container v-if="img1">
+        <v-layout row>
+        <v-img :src="img1"></v-img>
+        </v-layout>
     </v-container>
 
     <v-layout row>
-        <v-flex xs10 sm12 class="text-center">
+        <v-flex xs12>
+            <v-card>
+                <v-flex xs12 >
+    <v-layout row>
+        <v-flex xs12 class="text-center">
             <v-layout row>
                 {{ item.item }}
             </v-layout>
             <v-layout row>
                 {{ item.price }}
+            </v-layout> 
+            <v-layout>
+                {{item.description}}
             </v-layout>
             <v-layout row>
-                <v-flex xs12 offeset-sm3>
                                 <v-col class="d-flex" cols="12" sm="6">
                                     <v-select
                                     :items="numbers"
@@ -31,13 +42,12 @@
                                     v-model="number"
                                     ></v-select>
                                 </v-col>
-                            </v-flex>
+                            
             </v-layout>
-            <v-layout>
-                {{item.description}}
-            </v-layout>
+           
 
-            <v-col cols="12">
+            <v-layout row>
+                Delivery address
                          <v-textarea
                           v-model="deliver"
                           color="teal"
@@ -48,17 +58,29 @@
                          </div>
                        </template>
                         </v-textarea>
-                        </v-col>
+            </v-layout>
 
         </v-flex>
     </v-layout>
-
-
+                </v-flex>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </v-container>
 </template>
 
 <script>
 export default {
+    data: () => ({
+         
+            deliver: '',
+            wholeResponse: [],
+            img: '',
+            img0: '',
+            img1: ''
+
+        
+    }),
     props: ['id'],
     computed: {
         item() {
@@ -66,20 +88,29 @@ export default {
             
         },
 
- /*    img() {
+/* 
+     img() {
             return this.$store.getters.stocksfind(this.id).img
         
     },
-
+ 
     img0() {
-            return this.$store.getters.stocks(this.id).img0
+        return this.$store.getters.stocksfind(this.id).img0
         
-    },
-
+    }, */
+/* 
     img1() {
             return this.$store.getters.stocks(this.id).img1
         
-    }, */
+    },  */
+    },
+    mounted () {
+         
+            this.wholeResponse = this.$store.getters.stocksfind(this.id)
+            this.img = this.wholeResponse.img
+            this.img0 = this.wholeResponse.img0
+            this.img1 = this.wholeResponse.img1
+        
     }
 
 }
