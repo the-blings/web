91c99efc33d7461d9fd0c9f90d259dd075095cd1
C4Jay <choolakejay@gmail.com>
2020-03-07 11:55:01 +0530

    cart-work

--- src/components/ShoppingCart/Cart.vue
+++ src/components/ShoppingCart/Cart.vue
@@ -1,9 +1,26 @@
 <template>
     <v-container>
         <v-layout row class="text-center">
+            <v-flex xs12  class="text-center mb-8">
             <h1>Cart</h1>
-            <v-layout row v-for="item in items" :key="item.item">
-                {{item.item}}
+            </v-flex>
+            <v-layout row wrap v-for="item in items" :key="item.id" class="my-9">
+                <v-flex xs12 class="text-center">
+                    <v-card>
+                        <v-card-title>
+                            {{item.item}}
+                        </v-card-title>
+                        <v-card-text>
+                            <v-layout row>
+                            {{item.number}}
+                            </v-layout>
+                            <v-layout row>
+                            {{item.packaging}}
+                            </v-layout>
+                        </v-card-text>
+
+                    </v-card>
+                </v-flex>
             </v-layout>
         </v-layout>
     </v-container>
